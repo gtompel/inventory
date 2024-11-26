25f20326fecf8872803fd7af67d09eb608e88b75
tom_pel <quimpik@yandex.ru>
2024-11-26 20:30:24 +0300

    31:50

--- components/navbar/avatar.tsx
+++ components/navbar/avatar.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Avatar as AvatarComp, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 export default function Avatar() {
   return (
-    <div>Av
-      
-    </div>
+
+    <AvatarComp className='mr-2 h-8 w-8'
+    >
+        <AvatarImage
+        src='222.png'
+        alt="Laptev Dmitriy"
+         />
+         <AvatarFallback >
+            LD
+         </AvatarFallback>
+    </AvatarComp>
   )
 }
 
