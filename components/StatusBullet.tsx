0753bbe4ba247d1373c37d5fdbe8fecd711818ad
gtompel <quimpik@yandex.ru>
2024-11-27 17:06:23 +0300

    58:00

---
+++ components/StatusBullet.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TaskStatus } from './task-form/schema'
+
+type Props = {
+    status: TaskStatus
+}
+
+export default function StatusBullet(props: Props) {
+  return (
+    <div>StatusBullet</div>
+  )
+}
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TeamSwitcher  from './team-switcher'
 import Links from './links'
 import Search from './search'
-import Avatar from './Avatar'
+import Avatar from './avatar'
 
 
 function index() {
